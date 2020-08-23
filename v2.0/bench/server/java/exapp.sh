2f3efab6ff1b19a53d36038f3e69aaf966b07720
sachin sinha <sachin@iqlect.com>
2020-08-24 01:00:10 +0530

    adding server test for java

---
+++ v2.0/bench/server/java/exapp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH:lib
+EXE_NAME="java -cp .:lib/bangdb-client-java.jar -Djava.library.path="lib"  testbangdb.TestMain"
+
+# args = <num_items> <num_threads>
+# bash exapp.sh 100000 4
+echo "$EXE_NAME  $1 $2"
+$EXE_NAME  $1 $2
+
