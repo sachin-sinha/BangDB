d92f8bfaefc42d05c653b09a61e0bc25a40481b1
sachin sinha <sachin@iqlect.com>
2020-08-24 13:31:05 +0530

    removing the tmp files

--- v2.0/bench/embd/java/make-app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-javac -cp .:lib/bangdb-embd-java.jar testbangdb/GetThread.java testbangdb/PutThread.java testbangdb/Common.java testbangdb/StopWatch.java testbangdb/TestCases.java testbangdb/TestMain.java testbangdb/TestUnits.java
