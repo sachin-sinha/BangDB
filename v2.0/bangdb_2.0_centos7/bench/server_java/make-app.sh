c11ae7ec8f1197884f8731a6c020ed048fe97384
sachin sinha <sachin@iqlect.com>
2021-10-27 13:20:19 +0530

    moving binaries to release

--- v2.0/bangdb_2.0_centos7/bench/server_java/make-app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-javac -cp .:../../jars/bangdb-client-java.jar testbangdb/GetThread.java testbangdb/PutThread.java testbangdb/Common.java testbangdb/StopWatch.java testbangdb/TestCases.java testbangdb/TestMain.java testbangdb/TestUnits.java
--- v2.0/bangdb_2.0_centos8/bench/embd_cpp/bench/exapp.sh
+++
@@ -1,3 +0,0 @@
-EXE_NAME=./bench
-echo "$EXE_NAME  $1 $2 $3 $4"
-$EXE_NAME  $1 $2 $3 $4
