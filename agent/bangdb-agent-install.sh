ef0af4c09989a71373bd0d3ca4a31cf8e8794256
sachin sinha <sachin@iqlect.com>
2024-04-25 07:40:22 +0530

    fixing package issues

--- agent/bangdb-agent-install.sh
+++ agent/bangdb-agent-install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/sachin-sinha/BangDB/raw/master/agent/install_bdbagent.sh
-bash install_bdbagent.sh
-rm install_bdbagent.sh
+wget https://github.com/sachin-sinha/BangDB/raw/master/agent/install_bdbagent.sh && bash install_bdbagent.sh && rm install_bdbagent.sh
