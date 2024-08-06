948282e9c35c5e6683e659bb2f08acf25db0c595
Sachin Sinha <sachin@iqlect.com>
2024-08-06 17:54:46 +0530

    updating the agent bin

---
+++ agent/all/cpbin.sh
@@ -0,0 +1,14 @@
+cp ~/u18/bangdb-agent_s-2.0 bangdb-agent-ubuntu18/bin/
+cp ~/u18/bdbagent-2.0 bangdb-agent-ubuntu18/cli/
+cp ~/u20/bdbagent-2.0 bangdb-agent-ubuntu20/bin/
+cp ~/u20/bangdb-agent_s-2.0 bangdb-agent-ubuntu20/bin/
+cp ~/u22/bangdb-agent_s-2.0 bangdb-agent-ubuntu22/bin/
+cp ~/u22/bdbagent-2.0 bangdb-agent-ubuntu22/cli/
+cp ~/u24/bdbagent-2.0 bangdb-agent-ubuntu24/cli/
+cp ~/u24/bangdb-agent_s-2.0 bangdb-agent-ubuntu24/bin
+
+
+tar -czvf bangdb-agent-ubuntu18.tar.gz bangdb-agent-ubuntu18
+tar -czvf bangdb-agent-ubuntu20.tar.gz bangdb-agent-ubuntu20
+tar -czvf bangdb-agent-ubuntu22.tar.gz bangdb-agent-ubuntu22
+tar -czvf bangdb-agent-ubuntu24.tar.gz bangdb-agent-ubuntu24
